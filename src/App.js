35917effb6fbb0310dc21927653cc17fb35d48df
ppqqty <ppqqty@gmail.com>
2023-03-29 20:57:22 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'name': '이철희',
+const customers = [
+  {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '나동빈',
   'birthday': '961222',
   'gender': '남자',
   'job': '대학생'
-}
-
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동',
+    'birthday': '65997',
+    'gender': '남자',
+    'job': '도둑'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '968822',
+    'gender': '남자',
+    'job': '군인'
+  }
+]
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer 
+                key={c.id}
+                id={c.id} 
+                image={c.image} 
+                name={c.name} 
+                gender={c.gender} 
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
     );
   }
 }
